eec26247fd009b8dbda71af9650fdb63f611fc1a
Andrea Leardini <andre8244@gmail.com>
2025-07-16 10:59:19 +0200

    add redirect link on login

--- frontend/src/views/LoginRedirectView.vue
+++ frontend/src/views/LoginRedirectView.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
 import { useHandleSignInCallback } from '@logto/vue'
 import router from '@/router'
-import { NeSpinner } from '@nethesis/vue-components'
+import { NeLink, NeSpinner } from '@nethesis/vue-components'
+import { ref } from 'vue'
 
 useHandleSignInCallback(() => {
   // Redirect to home page on successful sign-in
   router.push('/')
 })
+
+const isRedirectMessageShown = ref(false)
+
+setTimeout(() => {
+  isRedirectMessageShown.value = true
+}, 3000)
+
+const goToDashboard = () => {
+  router.push({ name: 'dashboard' })
+}
 </script>
 
 <template>
-  <div>
-    <NeSpinner size="12" color="primary" />
+  <div class="flex flex-col items-start gap-6">
+    <NeSpinner size="12" color="white" />
+    <i18n-t
+      v-if="isRedirectMessageShown"
+      keypath="login.manual_redirect_to_dashboard"
+      tag="p"
+      scope="global"
+    >
+      <template #clickHereLink>
+        <NeLink @click="goToDashboard">
+          {{ $t('login.click_here') }}
+        </NeLink>
+      </template>
+    </i18n-t>
   </div>
 </template>
 
