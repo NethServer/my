737e17339894b00a694860b6e01cb5afba0b0f1f
Andrea Leardini <andre8244@gmail.com>
2025-07-17 15:58:17 +0200

    refactor environment files

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,7 +1,7 @@
 //  Copyright (C) 2025 Nethesis S.r.l.
 //  SPDX-License-Identifier: GPL-3.0-or-later
 
-export const PRODUCT_NAME = import.meta.env.VITE_PRODUCT_NAME
+export const PRODUCT_NAME = 'My Nethesis'
 
 // path requested by the OIDC provider to redirect the user after a successful login
 export const LOGIN_REDIRECT_URI = `${window.location.origin}/${import.meta.env.VITE_SIGNIN_REDIRECT_URI}`
@@ -13,4 +13,4 @@ export const SIGN_OUT_REDIRECT_URI = `${window.location.origin}/${import.meta.en
 export const LOGTO_ENDPOINT = import.meta.env.VITE_LOGTO_ENDPOINT
 export const LOGTO_APP_ID = import.meta.env.VITE_LOGTO_APP_ID
 
-export const API_URL = `${import.meta.env.VITE_API_SCHEME}://${import.meta.env.VITE_API_HOST}`
+export const API_URL = import.meta.env.VITE_API_BASE_URL
