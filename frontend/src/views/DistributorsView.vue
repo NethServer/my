3e85285f1f4711250c51282ef4d1e98cec8de196
Andrea Leardini <andre8244@gmail.com>
2026-01-20 08:59:01 +0100

    fix

--- frontend/src/views/DistributorsView.vue
+++ frontend/src/views/DistributorsView.vue
@@ -4,21 +4,70 @@
 -->
 
 <script setup lang="ts">
-import { NeButton, NeHeading } from '@nethesis/vue-components'
+import { NeButton, NeDropdown, NeHeading } from '@nethesis/vue-components'
 import DistributorsTable from '@/components/distributors/DistributorsTable.vue'
 import { computed, ref } from 'vue'
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import {
+  faChevronDown,
+  faCirclePlus,
+  faFileCsv,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { canManageDistributors } from '@/lib/permissions'
+import { canManageDistributors, canReadDistributors } from '@/lib/permissions'
 import { useDistributors } from '@/queries/distributors'
+import { useI18n } from 'vue-i18n'
+// import { downloadFile } from '@/lib/common' ////
+
+const { t } = useI18n()
+const { state, debouncedTextFilter } = useDistributors()
 
 const isShownCreateDistributorDrawer = ref(false)
-
-const { state, debouncedTextFilter } = useDistributors()
 
 const distributorsPage = computed(() => {
   return state.value.data?.distributors
 })
+
+function getBulkActionsMenuItems() {
+  return [
+    {
+      id: 'exportFilteredToPdf',
+      label: t('distributors.export_distributors_to_pdf'),
+      icon: faFilePdf,
+      // action: () => exportDistributors('pdf'), ////
+      disabled: !state.value.data?.distributors,
+    },
+    {
+      id: 'exportFilteredToCsv',
+      label: t('distributors.export_distributors_to_csv'),
+      icon: faFileCsv,
+      // action: () => exportDistributors('csv'), ////
+      disabled: !state.value.data?.distributors,
+    },
+  ]
+}
+
+//// implement after filters fix on backend
+// async function exportDistributors(format: 'pdf' | 'csv') {
+//   try {
+//     const exportData = await getExport(
+//       format,
+//       undefined,
+//       debouncedTextFilter.value,
+//       productFilter.value,
+//       createdByFilter.value,
+//       versionFilter.value,
+//       statusFilter.value,
+//       sortBy.value,
+//       sortDescending.value,
+//     )
+//     const fileName = `${t('distributors.title')}.${format}`
+//     downloadFile(exportData, fileName, format)
+//   } catch (error) {
+//     console.error(`Cannot export distributors to ${format}:`, error)
+//     throw error
+//   }
+// }
 </script>
 
 <template>
@@ -28,19 +77,44 @@ const distributorsPage = computed(() => {
       <div class="max-w-2xl text-gray-500 dark:text-gray-400">
         {{ $t('distributors.page_description') }}
       </div>
-      <!-- create distributor -->
-      <NeButton
-        v-if="canManageDistributors() && (distributorsPage?.length || debouncedTextFilter)"
-        kind="primary"
-        size="lg"
-        class="shrink-0"
-        @click="isShownCreateDistributorDrawer = true"
+      <!-- v-if condition is the opposite of empty state condition in DistributorsTable.vue -->
+      <div
+        v-if="!(state.status === 'success' && !distributorsPage?.length && !debouncedTextFilter)"
+        class="flex items-center gap-4"
       >
-        <template #prefix>
-          <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" />
-        </template>
-        {{ $t('distributors.create_distributor') }}
-      </NeButton>
+        <NeDropdown
+          :items="getBulkActionsMenuItems()"
+          align-to-right
+          :openMenuAriaLabel="$t('ne_dropdown.open_menu')"
+          v-if="canReadDistributors()"
+        >
+          >
+          <template #button>
+            <NeButton>
+              <template #suffix>
+                <FontAwesomeIcon
+                  :icon="faChevronDown"
+                  class="h-4 w-4"
+                  aria-hidden="true"
+                /> </template
+              >{{ $t('common.actions') }}</NeButton
+            >
+          </template>
+        </NeDropdown>
+        <!-- create distributor -->
+        <NeButton
+          v-if="canManageDistributors() && (distributorsPage?.length || debouncedTextFilter)"
+          kind="primary"
+          size="lg"
+          class="shrink-0"
+          @click="isShownCreateDistributorDrawer = true"
+        >
+          <template #prefix>
+            <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" />
+          </template>
+          {{ $t('distributors.create_distributor') }}
+        </NeButton>
+      </div>
     </div>
     <DistributorsTable
       :isShownCreateDistributorDrawer="isShownCreateDistributorDrawer"
