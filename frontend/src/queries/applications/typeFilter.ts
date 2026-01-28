ef17c9e08e955717e37d586ddcb0edd4876a1b1b
Andrea Leardini <andre8244@gmail.com>
2026-01-28 17:55:48 +0100

    add application page (wip)

--- frontend/src/queries/applications/typeFilter.ts
+++ frontend/src/queries/applications/typeFilter.ts
@@ -1,20 +1,17 @@
-//  Copyright (C) 2025 Nethesis S.r.l.
+//  Copyright (C) 2026 Nethesis S.r.l.
 //  SPDX-License-Identifier: GPL-3.0-or-later
 
-import {
-  getOrganizationFilter,
-  SYSTEM_ORGANIZATION_FILTER_KEY,
-} from '@/lib/systems/organizationFilter'
+import { APPLICATION_TYPE_FILTER_KEY, getTypeFilter } from '@/lib/applications/typeFilter'
 import { useLoginStore } from '@/stores/login'
 import { defineQuery, useQuery } from '@pinia/colada'
 
-export const useOrganizationFilter = defineQuery(() => {
+export const useTypeFilter = defineQuery(() => {
   const loginStore = useLoginStore()
 
   const { state, asyncStatus, ...rest } = useQuery({
-    key: () => [SYSTEM_ORGANIZATION_FILTER_KEY],
+    key: () => [APPLICATION_TYPE_FILTER_KEY],
     enabled: () => !!loginStore.jwtToken,
-    query: () => getOrganizationFilter(),
+    query: () => getTypeFilter(),
   })
 
   return {
