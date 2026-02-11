2bea16d336b165c28f99ff09c6d531be6522cb57
Andrea Leardini <andre8244@gmail.com>
2026-02-11 14:33:46 +0100

    add resellers & customers export

--- frontend/src/views/CustomersView.vue
+++ frontend/src/views/CustomersView.vue
@@ -4,21 +4,66 @@
 -->
 
 <script setup lang="ts">
-import { NeButton, NeHeading } from '@nethesis/vue-components'
+import { NeButton, NeDropdown, NeHeading } from '@nethesis/vue-components'
 import CustomersTable from '@/components/customers/CustomersTable.vue'
 import { computed, ref } from 'vue'
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import {
+  faChevronDown,
+  faCirclePlus,
+  faFileCsv,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { canManageCustomers } from '@/lib/permissions'
+import { canManageCustomers, canReadCustomers } from '@/lib/permissions'
 import { useCustomers } from '@/queries/organizations/customers'
+import { useI18n } from 'vue-i18n'
+import { getExport } from '@/lib/organizations/customers'
+import { downloadFile } from '@/lib/common'
+
+const { t } = useI18n()
+const { state, debouncedTextFilter, statusFilter, sortBy, sortDescending } = useCustomers()
 
 const isShownCreateCustomerDrawer = ref(false)
-
-const { state, debouncedTextFilter } = useCustomers()
 
 const customersPage = computed(() => {
   return state.value.data?.customers
 })
+
+function getBulkActionsMenuItems() {
+  return [
+    {
+      id: 'exportFilteredToPdf',
+      label: t('customers.export_customers_to_pdf'),
+      icon: faFilePdf,
+      action: () => exportCustomers('pdf'),
+      disabled: !state.value.data?.customers,
+    },
+    {
+      id: 'exportFilteredToCsv',
+      label: t('customers.export_customers_to_csv'),
+      icon: faFileCsv,
+      action: () => exportCustomers('csv'),
+      disabled: !state.value.data?.customers,
+    },
+  ]
+}
+
+async function exportCustomers(format: 'pdf' | 'csv') {
+  try {
+    const exportData = await getExport(
+      format,
+      debouncedTextFilter.value,
+      statusFilter.value,
+      sortBy.value,
+      sortDescending.value,
+    )
+    const fileName = `${t('customers.title')}.${format}`
+    downloadFile(exportData, fileName, format)
+  } catch (error) {
+    console.error(`Cannot export customers to ${format}:`, error)
+    throw error
+  }
+}
 </script>
 
 <template>
@@ -28,19 +73,43 @@ const customersPage = computed(() => {
       <div class="max-w-2xl text-gray-500 dark:text-gray-400">
         {{ $t('customers.page_description') }}
       </div>
-      <!-- create customer -->
-      <NeButton
-        v-if="canManageCustomers() && (customersPage?.length || debouncedTextFilter)"
-        kind="primary"
-        size="lg"
-        class="shrink-0"
-        @click="isShownCreateCustomerDrawer = true"
+      <!-- v-if condition is the opposite of empty state condition in CustomersTable.vue -->
+      <div
+        v-if="!(state.status === 'success' && !customersPage?.length && !debouncedTextFilter)"
+        class="flex items-center gap-4"
       >
-        <template #prefix>
-          <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" />
-        </template>
-        {{ $t('customers.create_customer') }}
-      </NeButton>
+        <NeDropdown
+          :items="getBulkActionsMenuItems()"
+          align-to-right
+          :openMenuAriaLabel="$t('ne_dropdown.open_menu')"
+          v-if="canReadCustomers()"
+        >
+          <template #button>
+            <NeButton>
+              <template #suffix>
+                <FontAwesomeIcon
+                  :icon="faChevronDown"
+                  class="h-4 w-4"
+                  aria-hidden="true"
+                /> </template
+              >{{ $t('common.actions') }}</NeButton
+            >
+          </template>
+        </NeDropdown>
+        <!-- create customer -->
+        <NeButton
+          v-if="canManageCustomers() && (customersPage?.length || debouncedTextFilter)"
+          kind="primary"
+          size="lg"
+          class="shrink-0"
+          @click="isShownCreateCustomerDrawer = true"
+        >
+          <template #prefix>
+            <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" />
+          </template>
+          {{ $t('customers.create_customer') }}
+        </NeButton>
+      </div>
     </div>
     <CustomersTable
       :isShownCreateCustomerDrawer="isShownCreateCustomerDrawer"
