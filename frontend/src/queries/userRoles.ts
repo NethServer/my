f4c8590557a8ea239116e5470c3032da7b6533c3
Andrea Leardini <andre8244@gmail.com>
2026-02-11 11:02:57 +0100

    reorganize pinia-colada queries

--- frontend/src/queries/userRoles.ts
+++ frontend/src/queries/userRoles.ts
@@ -1,25 +1,16 @@
 //  Copyright (C) 2026 Nethesis S.r.l.
 //  SPDX-License-Identifier: GPL-3.0-or-later
 
-// import { getUserRoles, USER_ROLES_KEY } from '@/lib/userRoles' ////
-// import { useLoginStore } from '@/stores/login'
-// import { defineQuery, useQuery } from '@pinia/colada'
+import { getUserRoles, USER_ROLES_KEY } from '@/lib/userRoles'
+import { useLoginStore } from '@/stores/login'
+import { defineQueryOptions } from '@pinia/colada'
 
-//// is this used?
+const loginStore = useLoginStore()
 
-////
-// export const useUserRoles = defineQuery(() => {
-//   const loginStore = useLoginStore()
+// This is not a "defineQuery" because we need an enabled condition that depends on the component that uses it (this query is typically used in a drawer that is not always open, and we don't want to fetch user roles if the drawer is not open)
 
-//   const { state, asyncStatus, ...rest } = useQuery({
-//     key: () => [USER_ROLES_KEY],
-//     enabled: () => !!loginStore.jwtToken,
-//     query: () => getUserRoles(),
-//   })
-
-//   return {
-//     ...rest,
-//     state,
-//     asyncStatus,
-//   }
-// })
+export const userRolesQuery = defineQueryOptions({
+  key: [USER_ROLES_KEY],
+  enabled: !!loginStore.jwtToken,
+  query: getUserRoles,
+})
