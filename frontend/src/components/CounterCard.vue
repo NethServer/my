b536d04c5f2c3a8266236103474c807f59a9d5ed
Andrea Leardini <andre8244@gmail.com>
2026-02-13 18:44:05 +0100

    improve CounterCard component

--- frontend/src/components/CounterCard.vue
+++ frontend/src/components/CounterCard.vue
@@ -15,12 +15,16 @@ const {
   icon = undefined,
   loading = false,
   skeletonLines = 2,
+  uppercaseTitle = true,
+  centeredCounter = true,
 } = defineProps<{
   title: string
   counter: number
   icon?: IconDefinition
   loading?: boolean
   skeletonLines?: number
+  uppercaseTitle?: boolean
+  centeredCounter?: boolean
 }>()
 
 const slots = useSlots()
@@ -32,7 +36,7 @@ const hasDefaultSlot = computed(() => !!slots.default)
   <NeCard>
     <NeSkeleton v-if="loading" :lines="skeletonLines" class="w-full" />
     <template v-else>
-      <div class="flex justify-between">
+      <div :class="['flex', centeredCounter ? 'flex-col gap-4' : 'justify-between']">
         <div class="flex items-center gap-3">
           <FontAwesomeIcon
             v-if="icon"
@@ -40,10 +44,15 @@ const hasDefaultSlot = computed(() => !!slots.default)
             class="size-5 text-gray-600 dark:text-gray-300"
           />
           <NeHeading tag="h6" class="text-gray-600 dark:text-gray-300">
-            {{ title }}
+            {{ uppercaseTitle ? title.toUpperCase() : title }}
           </NeHeading>
         </div>
-        <span class="text-3xl font-medium text-gray-900 dark:text-gray-50">
+        <span
+          :class="[
+            'self-center text-4xl font-medium text-indigo-700 dark:text-indigo-500',
+            { 'self-center': centeredCounter },
+          ]"
+        >
           {{ counter }}
         </span>
       </div>
