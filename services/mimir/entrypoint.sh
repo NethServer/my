e7a66605ed8cdcbea4b2802022cbe1bb3c2d262e
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2026-02-25 10:26:38 +0100

    Apply suggestions from code review

--- services/mimir/entrypoint.sh
+++ services/mimir/entrypoint.sh
@@ -7,5 +7,5 @@ export PORT
 echo "==> Expanding Mimir config..."
 envsubst < /etc/mimir/my.yaml.template > /tmp/mimir-config.yaml
 
-echo "==> Starting Mimir with alertmanager and ruler for alert support on port ${PORT}..."
+echo "==> Starting Mimir with alertmanager only for alert support on port ${PORT}..."
 exec /bin/mimir -config.file=/tmp/mimir-config.yaml
